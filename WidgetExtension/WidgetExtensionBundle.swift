67f03ac0a3e3e529526339c1267621d72dec4595
Vlad Paramonov <vlad_paramonov@me.com>
2023-05-21 22:10:53 +0100

    create widget extension

---
+++ WidgetExtension/WidgetExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetExtensionBundle.swift
+//  WidgetExtension
+//
+//  Created by Vlad Paramonov on 21.05.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetExtension()
+    }
+}
