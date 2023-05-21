7d7d094ef58d0491e69b75552cb140055cc0afcd
Vlad Paramonov <vlad_paramonov@me.com>
2023-05-21 22:07:46 +0100

    Initial Commit

---
+++ WidgetTestingKit/WidgetTestingKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTestingKitApp.swift
+//  WidgetTestingKit
+//
+//  Created by Vlad Paramonov on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTestingKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
